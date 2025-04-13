302a9e013907cf2cb1fa6850e5530a02c0cea6a5
agusquartz <agusquartz@gmail.com>
2025-04-13 17:57:39 -0300

    Update: Change Main to be up to date with desired execution flow

--- Main.java
+++ Main.java
@@ -1,14 +1,22 @@
+import java.util.LinkedList;
 
 public class Main{
     public static void main(String[] args){
         String path = "test.csv";
         FileManager fileManager = FileManager.getInstance();
         Parser parser = Parser.getInstance();
-        Process[] processes = parser.parse(fileManager.readFile(path));
+        LinkedList<BCP> BCPList = parser.parse(fileManager.readFile(path));
+
         FCFS fcfs = new FCFS();
-        Gantt g = new Gantt(processes);
+        Gantt gantt = new Gantt(BCPList);
+        Dispatcher dispatcher = new Dispatcher(BCPList, gantt);
 
         fileManager.checkOutput();
-        fileManager.appendToFile(g.generate(fcfs).toCSV("FCFS"));
+
+        // Logic of filtering and Execution of algorithms
+        if (dispatcher.run(fcfs)){
+            fileManager.appendToFile(gantt.toCSV("FCFS"));
+        }
+
     }
 }
