bbc6d197ac151af21e0f328e38995fc05d45f8c4
Andres Chyrnia <andreschyrnia@gmail.com>
2025-04-13 22:05:27 -0300

    Update: Modify Main so it tries more Algorithms

--- Main.java
+++ Main.java
@@ -8,6 +8,9 @@ public class Main{
         LinkedList<BCP> BCPList = parser.parse(fileManager.readFile(path));
 
         FCFS fcfs = new FCFS();
+        RR rr = new RR(4);
+        SJFE sjfe = new SJFE();
+        SJF sjf = new SJF();
         Gantt gantt = new Gantt(BCPList);
         Dispatcher dispatcher = new Dispatcher(BCPList, gantt);
 
@@ -17,6 +20,26 @@ public class Main{
         if (dispatcher.run(fcfs)){
             fileManager.appendToFile(gantt.toCSV("FCFS"));
         }
+        resetAll(BCPList);
+        if (dispatcher.run(rr)){
+            fileManager.appendToFile(gantt.toCSV("RR"));
+        }
+        resetAll(BCPList);
+        if (dispatcher.run(sjfe)){
+            fileManager.appendToFile(gantt.toCSV("SJFE"));
+        }
+        resetAll(BCPList);
+        if (dispatcher.run(sjf)){
+            fileManager.appendToFile(gantt.toCSV("SJF"));
+        }
 
+
+
+    }
+    private static void resetAll(LinkedList<BCP> list){
+        for(BCP b : list){
+            b.reset();
+        }
+        return;
     }
 }
