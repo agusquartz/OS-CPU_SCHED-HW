9a054cfe3bb1ac1b67ee214ad00c6c58bf4499c5
Andres Chyrnia <andreschyrnia@gmail.com>
2025-04-16 21:45:50 -0300

    WIP: Heavily modify run. Now it kinda mostly works.

--- Dispatcher.java
+++ Dispatcher.java
@@ -6,52 +6,61 @@ public class Dispatcher {
         this.chart = chart;
     }
     /* Run well... 'runs' the algorithm and fills the Gantt chart, then returns the String form of this chart
-     */
+    */
     public boolean run(Algorithm a){
         //simulates time
         //set up the loop
-        int currentTime = 0;
+        int currentTime = 0;    //inicia el tiempo
         BCP executing = a.apply(this.processList, currentTime);    //get first process to run
+        boolean firstTime = true; //in case we dont execute since the instant 0, keep track of when is the first time we process
+        GanttEntry e = null; //initialize GanttEntry to something
 
-        if (executing == null) {
-            System.out.println("Dispatcher: el algoritmo no devolvió ningún proceso para ejecutar.");
-            return false;
+        if(!(executing == null)){   //if we receive a process in instant 0
+            executing.setState(State.RUNNING);  //this process starts RUNNING in the first instant
+            executing.setFirstTime(currentTime);    //store in the BCP when this process started running
+            e = new GanttEntry(executing.getId(), currentTime);  //create a GanttEntry for this execution burst
+            firstTime = false;  //we started executing since instant 0, so firstTime is unnecesary
         }
-
-        executing.setState(State.RUNNING);  //this process starts RUNNING in the first instant
-        executing.setFirstTime(currentTime);    //store in the BCP when this process started running
-        GanttEntry e = new GanttEntry(executing.getId(), currentTime);  //create a GanttEntry for this execution burst
-        while(!allProcessesTerminated(this.processList)){
+        while(!allProcessesTerminated(this.processList)){   //so long as there are still processes to work on
             //at each time run the algorithm
-            BCP newExecuting = a.apply(this.processList,currentTime);
-
-            // Verification of next BCP
-            if(newExecuting != executing) {
-                
-                if(executing.getState() == State.RUNNING) {
-                    executing.setState(State.READY);
+            BCP newExecuting = a.apply(this.processList,currentTime);   //get the process to work on
+            if (newExecuting == null) { //if we don't have a process at this instant
+                currentTime++;  //in cases where there is no process at this time
+            }else{
+                if(firstTime){  //is this the first loop with a process to work on?
+                    e = new GanttEntry(newExecuting.getId(),currentTime);   //yes, create a Gantt Entry
+                    firstTime = false;  //now firstTime is unnecesary
                 }
+                // Verification of next BCP
+                if(newExecuting != executing && executing != null) { //is the process we got now, different to the one  we were working on last time?
+                    if(executing.getState() == State.RUNNING) { //if we were working on something, and we expulsed it
+                        executing.setState(State.READY);    //make sure the other process can come back
+                        e.setEndTime(currentTime);  //log the work we did
+                        this.chart.addEntry(e);
+                    }else{  //we have finished our work
+                    e.setEndTime(executing.getTerminationTime());  //log the work we did
+                    this.chart.addEntry(e);
+                    }
+                    e = new GanttEntry(newExecuting.getId(), currentTime);
+                }
+                executing = newExecuting;   //the current process is now the one we just got
+                                            
+                //save curTime
+                int timeBeforeUpdate = currentTime;
+                //increment currentTime
+                currentTime++;
+                //update the executing process bcp
+                updateBCP(executing, timeBeforeUpdate);
+                System.out.println("Bucle N°: "+currentTime);
+                chart.getEntries().stream().forEach(m -> System.out.println(m.getId()+" "+m.getStartTime()+" "+m.getEndTime()+"."+m.toString()));
             }
-            executing = newExecuting;
-
-            //has the running process changed?
-            if((executing.getState() == State.READY) || (executing.getState() == State.WAITING)){
-                //yes then close current GanttEntry
-                e.setEndTime(currentTime);
-                this.chart.addEntry(e);
-                //create a new GanttEntry with current executing processId
-                e = new GanttEntry(executing.getId(), currentTime);
-            }
-            //save curTime
-            int timeBeforeUpdate = currentTime;
-            //increment currentTime
-            currentTime++;
-            //update the executing process bcp
-            updateBCP(executing, timeBeforeUpdate);
         }
-        //close last GanttEntry
-        e.setEndTime(currentTime);
-        this.chart.addEntry(e);
+                //close last GanttEntry
+                e.setEndTime(executing.getTerminationTime());
+                this.chart.addEntry(e);
+                System.out.println("Bucle N°: "+currentTime);
+                chart.getEntries().stream().forEach(m -> System.out.println(m.getId()+" "+m.getStartTime()+" "+m.getEndTime()+"."+m.toString()));
+
         //all processes have terminated
         //return the GanttChar in csv form
         return true;
