8cd4d2dfbeb1748825a8513665e00b27ca66f40d
agusquartz <agusquartz@gmail.com>
2025-06-03 12:32:42 -0300

    Feature: add Page class implementation

--- src/mm/Page.java
+++ src/mm/Page.java
@@ -1,1 +1,19 @@
 package mm;
+
+public class Page {
+    private int pageNumber;
+    private int processId;
+
+    public Page(int pageNumber, int processId) {
+        this.pageNumber = pageNumber;
+        this.processId = processId;
+    }
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    public int getProcessId() {
+        return processId;
+    }
+}
