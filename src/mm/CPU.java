bdd438f5263d1f81b3de7c65207d518928b9ef08
Andres Chyrnia <andreschyrnia@gmail.com>
2025-06-03 22:43:23 -0300

    Feature: Add the CPU class
    The CPU class is in charge of isolating all paging logic from
    dispatcher. It receives all new instances related to paging and creates
    entries for the 'Gantt' chart. The method usePage() simulates trying to
    read from a page in RAM, and the following page fault that should occur
    if said page weren't there.
    
    The CPU class is bolted haphazardly to our program, and it's not really
    well integrated into the hierarchy. If we were to do it right, it should
    have a Dispatcher as one of its attributes, but that would entail a
    refactor that we can't really afford now with the deadline less than
    24hs away. It was easier this way, and it will work, so there's that.

---
+++ src/mm/CPU.java
@@ -0,0 +1,35 @@
+package mm;
+
+public class CPU {
+
+	public CPU(Memory ram, Memory virtual, PFH pageFaultMngr, PageTable table, PagingAlgorithm algo, LogManager logger){
+		this.ram = ram;
+		this.virtual = virtual;
+		this.pageFaultMngr = pageFaultMngr;
+		this.table = table;
+		this.algo = algo;
+		this.logger = logger;
+	}
+
+	public void usePage(Process p){
+		boolean fault = false;	//en principio, no hay una page fault
+		int pid = p.getId();	//dado el proceso, generamos pagina aleatoria a pedir
+		int page = (Math.random() * 100) % p.getPages();
+		//nos fijamos si la página aleatoria esta en ram mediante la PageTable
+		if(table.getPresenceValue(pid, page) == 0){	 //si la pagina no está en ram viene lo interesante
+			fault = true;	//avisamos a nuestro generador que hubo una pageFault
+			//invocamos a PFH, que va a correr el algoritmo y nos va a modificar la ram y la pagetable
+			pageFaultMngr.procesarFallo(pid, page, ram, virtual, table, algo); 
+		}
+		//hacemos una snapshot de ram y lo mandamos al logger
+		logger.addEntry(Arrays.copyOf(ram.getFrames(), fault);
+	}
+
+	//private vars
+	Memory ram;
+	Memory virtual;
+	PFH pageFaultMngr;
+	PageTable table;
+	PagingAlgorithm algo;
+	LogManager logger;
+}
