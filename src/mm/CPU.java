bfc68abc99df8accf4d9c6f145f748498bf5dc4e
Andres Chyrnia <andreschyrnia@gmail.com>
2025-06-04 14:37:21 -0300

    Update: Adjust usePage() so it passes a Page object to the algorithm

--- src/mm/CPU.java
+++ src/mm/CPU.java
@@ -5,7 +5,6 @@ public class CPU {
 	public CPU(Memory ram, Memory virtual, PageTable table, PagingAlgorithm algo, LogManager logger){
 		this.ram = ram;
 		this.virtual = virtual;
-		this.pageFaultMngr = pageFaultMngr;
 		this.table = table;
 		this.algo = algo;
 		this.logger = logger;
@@ -14,25 +13,23 @@ public class CPU {
 	public void usePage(Process p){
 		boolean fault = false;	//en principio, no hay una page fault
 		int pid = p.getId();	//dado el proceso, generamos pagina aleatoria a pedir
-		int page = (Math.random() * 100) % p.getPages();
+		int pageNum = (Math.random() * 100) % p.getPages();
+		Page page = new Page(pid, pageNum);
 		//nos fijamos si la página aleatoria esta en ram mediante la PageTable
 		if(table.getPresenceValue(pid, page) == 0){	 //si la pagina no está en ram viene lo interesante
 			fault = true;	//avisamos a nuestro generador que hubo una pageFault
 			//invocamos a PFH, que va a correr el algoritmo y nos va a modificar la ram y la pagetable
-			//pageFaultMngr.procesarFallo(pid, page, ram, virtual, table, algo); 
 			algo.pageFault(page);
 		} else{
-			pageHit(page);
+			algo.pageHit(page);
 		}
-		
 		//hacemos una snapshot de ram y lo mandamos al logger
-		logger.addEntry(Arrays.copyOf(ram.getFrames(), fault));
+		logger.addEntry(Arrays.copyOf(ram.getPages(), fault));
 	}
 
 	//private vars
 	Memory ram;
 	Memory virtual;
-	PFH pageFaultMngr;
 	PageTable table;
 	PagingAlgorithm algo;
 	LogManager logger;
